0bc6a4b284a26d2666aaef94b5bc9f44ceeac7b2
leaf-turner <wesley.shaw.475@my.csun.edu>
2021-12-17 15:28:04 -0800

    added error messages

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,45 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
     return '<p>logout</p>'
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
-    return render_template("sign_up.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        location = request.form.get('location')
+        password_1 = request.form.get('password1')
+        password_2 = request.form.get('password2')
+
+        #input integrity check
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(first_name) == 0:
+            flash('First Name cannot be empty.', category='error')
+        elif len(last_name) == 0:
+            flash('Last Name cannot be empty.', category='error')
+        # evntually make location a dropdown menu
+        elif len(location) == 0:
+            flash('Location cannot be empty.', category='error')
+        elif password_1 != password_2:
+            flash('Password\'s do not match.', category='error')
+        elif len(password_1) < 7:
+            flash('Password must be at least 8 characters long', category='error')
+        else:
+            #add user to db
+            flash('Account created!', category='success')
+
+    return render_template('sign_up.html')
 
